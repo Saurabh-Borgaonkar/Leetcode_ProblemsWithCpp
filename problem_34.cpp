f91c9268aee63da2ed2f4f8f24a26ce545f6c5a5
Saurabh-Borgaonkar <saurabhborgaonkar55@gmail.com>
2026-04-23 23:32:47 +0530

    problem 34

--- problem_34.cpp
+++ problem_34.cpp
@@ -1,36 +1,66 @@
-#include<iostream>
+// Function to find first and last position of target using binary search
+
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    int target;
-    int l=0,h=n-1;
-    bool f=false;
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    cout<<"\nEnter Target : ";
-    cin>>target;
-    int c=0;
-    while(l<=h){
-        int mid=(l+h)/2;
-        if(arr[mid]==target){
-            c=mid;
-            f=true;
-            break;
+
+vector<int> searchRange(vector<int>& nums, int target) {
+    int l = 0, h = nums.size() - 1;
+    int first = -1, last = -1;
+
+    // Find first occurrence
+    while(l <= h) {
+        int mid = l + (h - l) / 2;
+
+        if(nums[mid] == target) {
+            first = mid;
+            h = mid - 1;
         }
-        if(arr[mid]<target){
-            l=mid+1;
-        }else if(arr[mid]>target){
-            h=mid-1;
-        }
+        else if(nums[mid] < target)
+            l = mid + 1;
+        else
+            h = mid - 1;
     }
-   if(f){
-     cout<<c-1<<" "<<c;
-     }else{
-         cout<<" [-1,-1] ";
-        } 
-    
-        return 0;
+
+    l = 0;
+    h = nums.size() - 1;
+
+    // Find last occurrence
+    while(l <= h) {
+        int mid = l + (h - l) / 2;
+
+        if(nums[mid] == target) {
+            last = mid;
+            l = mid + 1;
+        }
+        else if(nums[mid] < target)
+            l = mid + 1;
+        else
+            h = mid - 1;
+    }
+
+    return {first, last};
+}
+
+int main() {
+    int n, target;
+
+    cout << "Enter size: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter sorted array: ";
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << "Enter target: ";
+    cin >> target;
+
+    vector<int> result = searchRange(nums, target);
+
+    cout << "Output: [" << result[0] << ", " << result[1] << "]";
+
+    return 0;
 }
