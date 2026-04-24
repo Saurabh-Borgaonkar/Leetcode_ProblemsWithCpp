3442d4f4dfabdf260bede9f6c14b7e937a3a3744
Saurabh-Borgaonkar <saurabhborgaonkar55@gmail.com>
2026-04-24 19:17:16 +0530

    problem 283

--- problem_283.cpp
+++ problem_283.cpp
@@ -1,20 +1,43 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-int main(){
-    int nums[]={0,1,0,2,13};
-    vector<int> ans;
-        int czero=0;
-        for(int i=0;i<5;i++){
-            if(nums[i]!=0){
-                ans.push_back(nums[i]);
-            }else czero++;
+
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    int n = nums.size();
+    int count = 0;
+    int ans = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(nums[i] == 1) {
+            count++;
+        } 
+        else {
+            ans = max(ans, count);
+            count = 0;
         }
-        for(int i=0;i<czero;i++){
-            ans.push_back(0);
-        }
-        cout<<czero<<endl;
-        for(int i=0;i<ans.size();i++){
-            cout<<ans[i]<<" ";
-        }
+    }
+
+    // handle case when array ends with 1s
+    if(ans < count)
+        return count;
+
+    return ans;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter elements (0 or 1): ";
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << "Maximum consecutive 1s: " 
+         << findMaxConsecutiveOnes(nums);
+
+    return 0;
 }
