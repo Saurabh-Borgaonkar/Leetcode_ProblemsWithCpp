275a1801066343ae5ecbe7409f254ce3e62c3fad
Saurabh-Borgaonkar <saurabhborgaonkar55@gmail.com>
2026-05-01 22:46:05 +0530

    problem  268 using set

--- problem_268.cpp
+++ problem_268.cpp
@@ -1,38 +1,61 @@
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+
+// class Solution {
+// public:
+//     int missingNumber(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int missing = 0;
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             if (nums[i] == missing) {
+//                 missing++;
+//             } else {
+//                 break;
+//             }
+//         }
+//         return missing;
+//     }
+// };
+
+// int main() {
+//     int n;
+//     cout << "Enter number of elements: ";
+//     cin >> n;
+
+//     vector<int> nums(n);
+//     cout << "Enter elements: ";
+//     for (int i = 0; i < n; i++) {
+//         cin >> nums[i];
+//     }
+
+//     Solution obj;
+//     cout << "Missing number is: " << obj.missingNumber(nums) << endl;
+
+//     return 0;
+// }
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <set>
 using namespace std;
 
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int missing = 0;
+int missingNumber(vector<int>& nums) {
+    set<int> s(nums.begin(), nums.end());
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == missing) {
-                missing++;
-            } else {
-                break;
-            }
+    int n = nums.size();
+
+    for(int i = 0; i <= n; i++) {
+        if(s.find(i) == s.end()) {
+            return i;
         }
-        return missing;
     }
-};
+
+    return -1;
+}
 
 int main() {
-    int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
-
-    vector<int> nums(n);
-    cout << "Enter elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> nums[i];
-    }
-
-    Solution obj;
-    cout << "Missing number is: " << obj.missingNumber(nums) << endl;
-
-    return 0;
+    vector<int> nums = {3, 0, 1};
+    cout << missingNumber(nums);
 }
