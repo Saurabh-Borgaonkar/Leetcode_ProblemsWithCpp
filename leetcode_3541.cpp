a883df22b11a5e790c3e1f76412ec8c04bd3cb01
Saurabh-Borgaonkar <saurabhborgaonkar55@gmail.com>
2026-08-06 00:12:47 +0530

    leetcode 3541

--- leetcode_3541.cpp
+++ leetcode_3541.cpp
@@ -1,20 +1,36 @@
-class Solution {
-public:
-    int maxFreqSum(string s) {
-        unordered_map<int,int>mp;
-        for(auto i : s){
-            mp[i]++;
-        }
-        long long maxVowels=0;
-        long long maxC=0;
-        for(auto i : mp){
-            if(string("aeiou").find(i.first)!=string::npos){
-                maxVowels=max(maxVowels,(long long)i.second);
-            }else{
-                maxC=max(maxC,(long long)i.second);
-            }
-        }
-        cout<<maxVowels+maxC;
-        return maxVowels+maxC;
+#include <iostream>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+int maxFreqSum(string s) {
+    unordered_map<char, int> mp;
+
+    for (char ch : s) {
+        mp[ch]++;
     }
-};
+
+    long long maxVowels = 0;
+    long long maxC = 0;
+
+    for (auto it : mp) {
+        if (string("aeiou").find(it.first) != string::npos) {
+            maxVowels = max(maxVowels, (long long)it.second);
+        } else {
+            maxC = max(maxC, (long long)it.second);
+        }
+    }
+
+    return maxVowels + maxC;
+}
+
+int main() {
+    string s;
+
+    cout << "Enter string: ";
+    cin >> s;
+
+    cout << "Answer: " << maxFreqSum(s) << endl;
+
+    return 0;
+}
