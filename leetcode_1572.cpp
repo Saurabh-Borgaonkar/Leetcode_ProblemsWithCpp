b9428bef84f7bbab5e7468f8e62e3ed6a1fe5778
Saurabh-Borgaonkar <saurabhborgaonkar55@gmail.com>
2026-09-17 00:21:53 +0530

    1572 leetcode

---
+++ leetcode_1572.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int diagonalSum(vector<vector<int>>& mat) {
+    int n = mat.size();
+    int sum = 0;
+
+    for (int i = 0; i < n; i++) {
+        sum += mat[i][i];           // Primary diagonal
+        sum += mat[i][n - 1 - i];   // Secondary diagonal
+    }
+
+    // Remove duplicate center element
+    if (n % 2 == 1) {
+        sum -= mat[n / 2][n / 2];
+    }
+
+    return sum;
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<vector<int>> mat(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    cout << diagonalSum(mat) << endl;
+
+    return 0;
+}
